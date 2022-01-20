cb2d1967be3c1aacbd34abeac7acc2f867edecde
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-01-20 00:18:44 -0500

    first commit

---
+++ src/main/java/manman/ohmyjava/Concatenation_1929.java
@@ -0,0 +1,24 @@
+package manman.ohmyjava;
+
+ 
+public class Concatenation_1929 
+{
+    public static void main( String[] args )
+    {
+       
+    	 int[] nums = {1,2,3};
+    	 
+    	 int len = nums.length;    	
+    	 int[] rest = new int[len*2];
+    	 
+    	 for(int i=0; i < len ; i++) {
+    		 rest[i] = nums[i];
+    		 rest[i+len] = nums[i];    		 
+    	 }
+    	 
+    	 for(int i = 0 ; i < rest.length ; i++) {
+    		 System.out.println(rest[i]);
+    	 }
+    }    
+    
+}
---
+++ src/test/java/manman/ohmyjava/AppTest.java
@@ -0,0 +1,20 @@
+package manman.ohmyjava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
