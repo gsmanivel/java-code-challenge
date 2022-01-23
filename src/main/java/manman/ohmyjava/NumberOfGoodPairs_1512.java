304f2b5d1b190fb65abd322764a85a48c992dfa7
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-01-22 23:11:41 -0500

    jan22

--- src/main/java/manman/ohmyjava/NumberOfGoodPairs_1512.java
+++ src/main/java/manman/ohmyjava/NumberOfGoodPairs_1512.java
@@ -5,20 +5,19 @@ import java.util.List;
 
 public class NumberOfGoodPairs_1512 {
     public static void main(String [] args) {
-        getVal().stream().forEach(val -> System.out.println(val));
+       System.out.println(getVal());
     }
 
 
-    public static List<Boolean> getVal() {
+    public static int getVal() {
         int[] nums = {1,2,3,1,1,3};
         int goodPair=0;
-        for(int i =0; i< nums.length;i++){
-            for(int j=i+1; j<nums.length;j++){
-                if(nums[i] == nums[j])
-                    goodPair +=1;
+        for(int i =0; i< nums.length;i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j])
+                    goodPair += 1;
             }
         }
-        System.out.println(goodPair);
-        return null;
+        return goodPair;
     }
 }
