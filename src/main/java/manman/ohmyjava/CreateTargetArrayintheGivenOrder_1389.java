3bbc1b6b432d750d06bf28684842fa62deedf1f0
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-01-28 20:25:14 -0500

    jan28

--- src/main/java/manman/ohmyjava/CreateTargetArrayintheGivenOrder_1389.java
+++ src/main/java/manman/ohmyjava/CreateTargetArrayintheGivenOrder_1389.java
@@ -1,39 +1,44 @@
 package manman.ohmyjava;
 
+import java.util.Arrays;
+
 public class CreateTargetArrayintheGivenOrder_1389 {
     //    https://leetcode.com/problems/create-target-array-in-the-given-order/
     public static void main(String[] args) {
-        int[] nums = {0, 1, 2, 3, 4};
-        int[] index = {0, 1, 2, 2, 1};
-        new CreateTargetArrayintheGivenOrder_1389().createTargetArray(nums, index);
+        int[] nums = {2,2,7,5,7};
+        int[] index = {0,1,1,0,4};
+        int[] result = new CreateTargetArrayintheGivenOrder_1389().createTargetArray(nums, index);
+        Arrays.stream(result).forEach(item -> System.out.println(item));
     }
 
     public int[] createTargetArray(int[] nums, int[] index) {
         boolean flag = false;
-        int[] result = new int[nums.length];
-        int[] tempArray = new int[nums.length];
+        boolean straightFlow = true;
+        int[] regularArray = new int[nums.length];
+        int[] finalResult = new int[nums.length];
         for (int i = 0; i < index.length; i++) {
             int indexValue = index[i];
-            if (indexValue == i) {
-                result[i] = nums[i];
-            } else {
+            if (indexValue == i)
+                regularArray[i] = nums[i];
+            else {
+                straightFlow= false;
                 flag = true;
-                for(int j=0;j<nums.length;j++){
-                    if(j==indexValue){
-                        tempArray[j] = nums[j];
+            }
+
+            if (flag) {
+                boolean insertFlag=false;
+                for(int loop2=0; loop2<nums.length;loop2++){
+                    if(!(loop2 == indexValue))
+                            finalResult[loop2] = insertFlag ? regularArray[loop2-1] : regularArray[loop2];
+                    else {
+                        insertFlag=true;
+                        finalResult[loop2] = nums[i];
                     }
                 }
-            }
-            if (flag) {
-
-                result = tempArray;
+                flag=false;
+                regularArray = Arrays.copyOf(finalResult,nums.length);
             }
         }
-        return result;
-    }
-
-    public int[] insertArray(){
-
-        return new int[0];
+        return regularArray;
     }
 }
