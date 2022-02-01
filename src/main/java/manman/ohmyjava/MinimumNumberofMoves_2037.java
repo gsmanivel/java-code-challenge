45a84576236c5e5fc7e17927e0e2594d768d9dbd
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-01-31 20:00:15 -0500

    jan31

---
+++ src/main/java/manman/ohmyjava/MinimumNumberofMoves_2037.java
@@ -0,0 +1,25 @@
+package manman.ohmyjava;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MinimumNumberofMoves_2037 {
+    public static void main(String[] args) {
+        MinimumNumberofMoves_2037 obj = new MinimumNumberofMoves_2037();
+        int[] seats= {4,1,5,9};             // 1 4 5 9
+        int[] students = {1,3,2,6};         // 1,2,3,6
+        System.out.println(obj.minMovesToSeat(seats,students));
+    }
+
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int result=0;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        for(int i=0;i<students.length;i++){
+            result += Math.abs(students[i] - seats[i]);
+        }
+        return result;
+    }
+}
